fd12090cad32edafc3ba5d83a1f8164afc45db47
Oasisktesh <emil.ktesh@gmail.com>
2023-09-19 12:43:41 +0200

    labupdatering 4

--- Code-Along/l015.py
+++ Code-Along/l015.py
@@ -1,12 +1,13 @@
-try:
-    my_int = int(input("input an integer ")) 
-except ValueError:
-    print("Value error, my int must be an integer.")
-else: 
-    print(f"my_int = {my_int}")  
-    
-#när man försöker anropa en variable eller string som
-# inte finns så är det en typ av runtim error
-#syntax error du har skrivit något fel.
-# runtime error när något inte fungerar som det ska inom sparning 
-# av minnet/value error
+def input_int(prompt):
+   
+    while True:
+       try:
+          return int(input(prompt))
+       except ValueError:
+          print("Value error, my int must be an integer.")
+
+age = input_int("input age")
+length = input_int("input length")
+weight = input_int("input weight")
+print(f"age = {age}, length = {length}, weight = {weight}")
+   
